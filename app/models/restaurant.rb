4100f80ffefd40f39843195249c1a0c6333dee96
Sergey Onoyko <sergey.v.onoyko@gmail.com>
2021-02-17 12:22:44 +0100

    restaurant and review models added with clean style

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :category, inclusion: %w(chinese italian japanese french belgian)
-  validates :name, :address, presence: true
+  validates :name, :address, :category, presence: true
 end
